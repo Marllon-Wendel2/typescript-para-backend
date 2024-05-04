8c600b663bbf0fb129ac714ff46dcb234f0d6834
Marllon Wendel <m.w.p.ferreira@outlook.com>
2024-05-04 09:50:45 -0300

    TipoPet

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,10 +1,13 @@
 import  {Request, Response } from "express";
+import type TipoPet from "../tipo/TipoPet";
 
-let listaDePets = [];
+let listaDePets: Array<TipoPet> = [];
 
 export default class PetController {
     criaPet(req:Request, res:Response) {
-        const novoPet = req.body;
+        const {id,adotado,especie,idade,nome} = <TipoPet>req.body;
+        const novoPet: TipoPet = {id,adotado,especie,idade,nome};
+        
         listaDePets.push(novoPet);
         return res.status(201).json(novoPet);
     }
---
+++ src/tipo/TipoPet.ts
@@ -0,0 +1,8 @@
+type TipoPet = {
+    id: number;
+    nome: string;
+    especie: string;
+    adotado: boolean;
+    idade: number;
+};
+export default TipoPet
